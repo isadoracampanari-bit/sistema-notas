8652eb00c4293eb1cf8cfe0163d19a17ddac089f
isacampanari <isadora.campanari@edu.senai.br>
2026-05-20 10:24:04 -0300

    feat: adiciona notas e cáculo de média

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main () {
  //Entrada - declaração das variáveis
 string nomes[20];
 int qtdAlunos;
+float notas[20][5];
+float media[20];
+int qtdDisciplinas;
 
 //Processamento
 cout << "=== SISTEMA DE NOTAS v4.0 ===" << endl;
@@ -20,6 +23,24 @@ cin.ignore();
 for (int i = 0; i < qtdAlunos; i++) {
     cout << "Nome do aluno: " << i + 1<< ": ";
     getline (cin, nomes[i]);
+}
+
+do{
+    cout << "\nQuantidade de disciplinas (1 a 5): " << endl;
+    cin >> qtdDisciplinas;
+} while (qtdDisciplinas < 1 || qtdDisciplinas > 5);
+
+for (int i = 0; i < qtdAlunos; i++){
+    cout << "\nNotas de " << nomes[i] << ":" << endl;
+    float soma = 0;
+    for (int j = 0; j < qtdDisciplinas; j++) {
+        do {
+            cout << "Disciplina " << j + 1 << " (1 a 5): ";
+            cin >> notas[i][j];
+        } while (notas[i][j] < 1 || notas[i][j] > 5);
+        soma += notas[i][j];
+    }
+    media[i] = soma / qtdDisciplinas;
 }
 
 //Saída
